20ff02f07ebda2601208d12eae5e666c2949f2f3
Terrance Rose Jr <trose618@gmail.com>
2018-10-20 20:51:29 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,48 @@
+def using_push(array, string)
+  
+end
+
+def using_push(array, string)
+  
+end
+
+def using_unshift(array)
+  
+end
+
+def using_pop(array)
+  
+end
+
+def pop_with_args(array)
+  
+end
+
+def using_shift(array)
+  
+end
+
+def shift_with_args(array)
+  
+end
+
+def using_concat
+  
+end
+
+def using_insert(array, new_element)
+  
+end
+
+def using_uniq(array)
+  
+end
+
+def using_flatten(array)
+  
+end
+
+def using_delete(array, string)
+  
+end
+
